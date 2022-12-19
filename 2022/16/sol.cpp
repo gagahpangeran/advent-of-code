d6080ded17d8e119053b9f7371286337af24f083
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2022-12-19 16:34:52 +0700

    add day 16 part 2 solution

--- 2022/16/sol.cpp
+++ 2022/16/sol.cpp
@@ -6,6 +6,7 @@ using namespace std;
 map<string, vector<string>> gr;
 map<string, int> rt;
 map<pair<string, string>, int> apsp;
+vector<string> nozero;
 
 int BFS(string start, string end)
 {
@@ -47,7 +48,7 @@ int dist(string x, string y)
   return apsp[{y, x}];
 };
 
-map<tuple<string, int, set<string>>, int> dp;
+map<tuple<string, int, set<string>>, int> memo;
 
 int solve(string cur, int time, set<string> next)
 {
@@ -56,8 +57,8 @@ int solve(string cur, int time, set<string> next)
   if (next.empty())
     return 0;
 
-  if (dp.count({cur, time, next}) > 0)
-    return dp[{cur, time, next}];
+  if (memo.count({cur, time, next}) > 0)
+    return memo[{cur, time, next}];
 
   int res = 0;
 
@@ -72,9 +73,45 @@ int solve(string cur, int time, set<string> next)
     res = max(nr, res);
   }
 
-  dp[{cur, time, next}] = res;
+  memo[{cur, time, next}] = res;
 
   return res;
+}
+
+void solve1()
+{
+  int res = solve("AA", 30, set<string>(nozero.begin(), nozero.end()));
+  cout << res << nl;
+}
+
+void solve2()
+{
+  int n = nozero.size();
+  int all = pow(2, n);
+
+  int mx = -1;
+
+  for (int i = 0; i < all; i++)
+  {
+    string bs = bitset<16>(i).to_string().substr(16 - n, n);
+
+    assert(n == bs.size());
+
+    vector<string> seta, setb;
+    for (int j = 0; j < n; j++)
+    {
+      if (bs[j] == '1')
+        seta.push_back(nozero[j]);
+      else
+        setb.push_back(nozero[j]);
+    }
+
+    int res1 = solve("AA", 26, set(seta.begin(), seta.end()));
+    int res2 = solve("AA", 26, set(setb.begin(), setb.end()));
+    mx = max(mx, res1 + res2);
+  }
+
+  cout << mx << nl;
 }
 
 int main()
@@ -116,15 +153,14 @@ int main()
       apsp[{vl[i], vl[j]}] = BFS(vl[i], vl[j]);
   }
 
-  set<string> nozero;
   for (auto v : vl)
   {
     if (rt[v] > 0)
-      nozero.insert(v);
+      nozero.push_back(v);
   }
 
-  int res = solve("AA", 30, nozero);
-  cout << res << nl;
+  solve1();
+  solve2();
 
   return 0;
 }
