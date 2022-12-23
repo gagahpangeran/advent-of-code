85421e07bc546598e08d1bda577ad8509770d4c1
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2022-12-23 15:03:48 +0700

    add day 22 part 2 solution

--- 2022/22/sol.cpp
+++ 2022/22/sol.cpp
@@ -5,6 +5,8 @@ using namespace std;
 #define fi first
 #define se second
 
+typedef pair<int, int> pii;
+
 int main()
 {
   ios::sync_with_stdio(0);
@@ -51,7 +53,7 @@ int main()
 
   int n = grid.size(), m = grid[0].size();
 
-  vector<pair<int, int>> boundrow(n, {0, m - 1});
+  vector<pii> boundrow(n, {0, m - 1});
 
   for (int i = 0; i < n; i++)
   {
@@ -74,7 +76,7 @@ int main()
     }
   }
 
-  vector<pair<int, int>> boundcol(m, {0, n - 1});
+  vector<pii> boundcol(m, {0, n - 1});
 
   for (int j = 0; j < m; j++)
   {
@@ -97,10 +99,36 @@ int main()
     }
   }
 
-  char face = '>';
+  vector<char> faces = {'>', 'v', '<', '^'};
+
+  auto FaceVal = [&](char fa)
+  {
+    return (int)(find(faces.begin(), faces.end(), fa) - faces.begin());
+  };
+
+  auto Turn = [&](char tu, char fa)
+  {
+    int idx = FaceVal(fa);
+    int fsz = faces.size();
+
+    auto Mod = [&](int a, int b)
+    {
+      return (a % b + b) % b;
+    };
+
+    if (tu == 'R')
+      return faces[Mod(idx + 1, fsz)];
+
+    if (tu == 'L')
+      return faces[Mod(idx - 1, fsz)];
+
+    assert(false);
+  };
 
   int i = 0;
   int j = boundrow[0].fi;
+
+  char face = faces[0];
 
   int ist = 0, itu = 0;
 
@@ -154,35 +182,204 @@ int main()
     char tu = turn[itu];
     itu++;
 
-    map<char, char> turnright = {
-        {'>', 'v'},
-        {'v', '<'},
-        {'<', '^'},
-        {'^', '>'},
-    };
-
-    map<char, char> turnleft = {
-        {'>', '^'},
-        {'^', '<'},
-        {'<', 'v'},
-        {'v', '>'},
-    };
-
-    if (tu == 'R')
-      face = turnright[face];
-    else if (tu == 'L')
-      face = turnleft[face];
+    face = Turn(tu, face);
   }
 
-  map<char, int> faceval = {
-      {'>', 0},
-      {'v', 1},
-      {'<', 2},
-      {'^', 3},
+  int res = (i + 1) * 1000 + (j + 1) * 4 + FaceVal(face);
+  cout << res << nl;
+
+  // solution bellow only works for input file only (does not works for sample file)
+
+  int len = 50;
+
+  vector<pii> sarea = {
+      {0, len},
+      {0, 2 * len},
+      {len, len},
+      {2 * len, 0},
+      {2 * len, len},
+      {3 * len, 0},
   };
 
-  int res = (i + 1) * 1000 + (j + 1) * 4 + faceval[face];
-  cout << res << nl;
+  vector<vector<int>> garea(n, vector<int>(m, 0));
+
+  for (int iarea = 0; iarea < sarea.size(); iarea++)
+  {
+    auto [u, v] = sarea[iarea];
+    for (int i = 0; i < len; i++)
+      for (int j = 0; j < len; j++)
+        garea[u + i][v + j] = iarea + 1;
+  }
+
+  i = 0;
+  j = boundrow[0].fi;
+
+  face = faces[0];
+
+  ist = 0, itu = 0;
+
+  for (;;)
+  {
+    if (ist >= step.size())
+      break;
+
+    int st = step[ist];
+    ist++;
+
+    while (st--)
+    {
+      map<char, pii> nx = {
+          {'>', {0, 1}},
+          {'<', {0, -1}},
+          {'v', {1, 0}},
+          {'^', {-1, 0}},
+      };
+
+      int nexti = i + nx[face].fi, nextj = j + nx[face].se;
+      char nextface = face;
+
+      auto [l, r] = boundrow[i];
+      auto [u, d] = boundcol[j];
+
+      if (nextj < l || nextj > r || nexti > d || nexti < u)
+      {
+        int narea = garea[i][j];
+
+        if (narea == 1 && face == '<')
+        {
+          assert(j == len);
+          nextj = 0;
+          nexti = 2 * len + (len - i - 1);
+          assert(garea[nexti][nextj] == 4);
+          nextface = '>';
+        }
+        else if (narea == 1 && face == '^')
+        {
+          assert(i == 0);
+          nextj = 0;
+          nexti = 3 * len + (j - len);
+          assert(garea[nexti][nextj] == 6);
+          nextface = '>';
+        }
+        else if (narea == 2 && face == '^')
+        {
+          assert(i == 0);
+          nexti = 4 * len - 1;
+          nextj = j - (2 * len);
+          assert(garea[nexti][nextj] == 6);
+          nextface = '^';
+        }
+        else if (narea == 2 && face == '>')
+        {
+          assert(j == 3 * len - 1);
+          nextj = 2 * len - 1;
+          nexti = 2 * len + (len - i - 1);
+          assert(garea[nexti][nextj] == 5);
+          nextface = '<';
+        }
+        else if (narea == 2 && face == 'v')
+        {
+          assert(i == len - 1);
+          nextj = 2 * len - 1;
+          nexti = len + (j - 2 * len);
+          assert(garea[nexti][nextj] == 3);
+          nextface = '<';
+        }
+        else if (narea == 3 && face == '<')
+        {
+          assert(j == len);
+          nexti = 2 * len;
+          nextj = i - len;
+          assert(garea[nexti][nextj] == 4);
+          nextface = 'v';
+        }
+        else if (narea == 3 && face == '>')
+        {
+          assert(j == 2 * len - 1);
+          nexti = len - 1;
+          nextj = 2 * len + (i - len);
+          assert(garea[nexti][nextj] == 2);
+          nextface = '^';
+        }
+        else if (narea == 4 && face == '^')
+        {
+          assert(i == 2 * len);
+          nextj = len;
+          nexti = len + j;
+          assert(garea[nexti][nextj] == 3);
+          nextface = '>';
+        }
+        else if (narea == 4 && face == '<')
+        {
+          assert(j == 0);
+          nextj = len;
+          nexti = 3 * len - 1 - i;
+          assert(garea[nexti][nextj] == 1);
+          nextface = '>';
+        }
+        else if (narea == 5 && face == '>')
+        {
+          assert(j == 2 * len - 1);
+          nextj = 3 * len - 1;
+          nexti = 3 * len - 1 - i;
+          assert(garea[nexti][nextj] == 2);
+          nextface = '<';
+        }
+        else if (narea == 5 && face == 'v')
+        {
+          assert(i == 3 * len - 1);
+          nextj = len - 1;
+          nexti = 3 * len + (j - len);
+          assert(garea[nexti][nextj] == 6);
+          nextface = '<';
+        }
+        else if (narea == 6 && face == '<')
+        {
+          assert(j == 0);
+          nexti = 0;
+          nextj = len + (i - 3 * len);
+          assert(garea[nexti][nextj] == 1);
+          nextface = 'v';
+        }
+        else if (narea == 6 && face == '>')
+        {
+          assert(j == len - 1);
+          nexti = 3 * len - 1;
+          nextj = len + (i - 3 * len);
+          assert(garea[nexti][nextj] == 5);
+          nextface = '^';
+        }
+        else if (narea == 6 && face == 'v')
+        {
+          assert(i == 4 * len - 1);
+          nexti = 0;
+          nextj = 2 * len + j;
+          assert(garea[nexti][nextj] == 2);
+          nextface = 'v';
+        }
+        else
+          assert(false);
+      }
+
+      if (grid[nexti][nextj] == '#')
+        break;
+
+      i = nexti;
+      j = nextj;
+      face = nextface;
+    }
+
+    if (itu >= turn.size())
+      break;
+
+    char tu = turn[itu];
+    itu++;
+
+    face = Turn(tu, face);
+  }
+
+  int res2 = (i + 1) * 1000 + (j + 1) * 4 + FaceVal(face);
+  cout << res2 << nl;
 
   return 0;
 }
