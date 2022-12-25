532253b1fdff9cd9488fb94022280ef915256b96
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2022-12-25 23:54:52 +0700

    add day 20 part 2 solution

--- 2022/20/sol.cpp
+++ 2022/20/sol.cpp
@@ -5,45 +5,37 @@ using namespace std;
 #define fi first
 #define se second
 
-typedef vector<int> vi;
-typedef pair<int, int> pii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
 
-void p(vi &v)
+void p(vll &v)
 {
   for (auto i : v)
     cout << i << " ";
   cout << nl;
 }
 
-void p(vector<pii> &v)
+void p(vector<pll> &v)
 {
   for (auto &[i, j] : v)
     cout << "(" << i << ", " << j << ") ";
   cout << nl;
 }
 
-int mod(int a, int b)
+ll mod(ll a, ll b)
 {
   return (a % b + b) % b;
 };
 
-int main()
+void mixing(vll &v, ll t)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+  ll n = v.size();
 
-  int x;
-  vi v;
+  vector<pll> u;
+  pll zero;
 
-  while (cin >> x)
-    v.push_back(x);
-
-  int n = v.size();
-
-  vector<pii> u;
-  pii zero;
-
-  for (int i = 0; i < n; i++)
+  for (ll i = 0; i < n; i++)
   {
     if (v[i] == 0)
       zero = {v[i], i};
@@ -51,34 +43,61 @@ int main()
     u.push_back({v[i], i});
   }
 
-  for (int i = 0; i < n; i++)
+  while (t--)
   {
-    int pos = find(u.begin(), u.end(), pair{v[i], i}) - u.begin();
-    int npos = mod(pos + u[pos].fi, n);
 
-    int num = u[pos].fi;
-    int nx = num > 0 ? 1 : -1;
-    int step = abs(num);
-    int cur = pos;
-
-    while (step--)
+    for (ll i = 0; i < n; i++)
     {
-      int next = mod(cur + nx, n);
-      swap(u[cur], u[next]);
-      cur = next;
+      ll pos = find(u.begin(), u.end(), pair{v[i], i}) - u.begin();
+      ll npos = mod(pos + u[pos].fi, n - 1);
+
+      pll cur = u[pos];
+
+      vector<pll> nextu;
+      for (ll j = 0; j < n; j++)
+      {
+        if (nextu.size() == npos)
+          nextu.push_back(cur);
+
+        if (u[j] != cur)
+          nextu.push_back(u[j]);
+      }
+
+      assert(u.size() == nextu.size());
+      u = nextu;
     }
   }
 
-  int zpos = find(u.begin(), u.end(), zero) - u.begin();
+  ll zpos = find(u.begin(), u.end(), zero) - u.begin();
 
-  int res = 0;
-  for (int i = 1; i <= 3; i++)
+  ll res = 0;
+  for (ll i = 1; i <= 3; i++)
   {
-    int t = mod(zpos + (i * 1e3), n);
+    ll t = mod(zpos + (i * 1e3), n);
     res += u[t].fi;
   }
 
   cout << res << nl;
+}
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  ll x;
+  vll v;
+
+  while (cin >> x)
+    v.push_back(x);
+
+  mixing(v, 1);
+
+  const ll key = 811589153;
+  for (ll i = 0; i < v.size(); i++)
+    v[i] *= key;
+
+  mixing(v, 10);
 
   return 0;
 }
